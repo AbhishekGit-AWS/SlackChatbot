95e6486ba7b0ac5a192f3ab3b45bd7c3e26ce544
Abhsishek Verma <aviverma1101@gmail.com>
2020-07-02 13:49:42 +0530

    CF Template to deploy app to tst AutoSaling.

--- phpStressApp/index.php
+++ phpStressApp/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Elastic Load Balancing</title>
+<title>Load servers to display Autoscaling and LoadBalancing</title>
 <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
 
 </head>
